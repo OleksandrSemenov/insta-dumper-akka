eff3f609b6656fe8de36da9ccb42997abb97398b
chuiykPasha <p4y10k@gmail.com>
2018-05-22 15:22:49 +0300

    merge extandDb to master

--- src/main/java/scanner/rest/FakeUserRestController.java
+++ src/main/java/scanner/rest/FakeUserRestController.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import scanner.entities.FakeUser;
 import scanner.repository.FakeUserRepository;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/fakeUsers")
@@ -38,6 +36,40 @@ public class FakeUserRestController {
         }
 
         fakeUserRepository.save(fakeUser);
+        return new ResponseEntity<>(fakeUser, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<FakeUser> update(@RequestBody @Valid FakeUser fakeUser) {
+        if(fakeUser == null || fakeUser.getId() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        fakeUserRepository.save(fakeUser);
+        return new ResponseEntity<>(fakeUser, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<FakeUser> delete(@PathVariable("id") Integer id) {
+        FakeUser fakeUser = fakeUserRepository.getOne(id);
+
+        if(fakeUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        fakeUserRepository.delete(fakeUser);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<FakeUser> getById(@PathVariable("id") Integer id) {
+        Optional<FakeUser> optional = fakeUserRepository.findById(id);
+        FakeUser fakeUser = optional.get();
+        
+        if(fakeUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(fakeUser, HttpStatus.OK);
     }
 }
