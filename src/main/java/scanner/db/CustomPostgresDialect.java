953138d3c347fe7352854aa4ba5506920ac8cc2b
chuiykPasha <p4y10k@gmail.com>
2018-05-31 15:20:41 +0300

    change deprecated posgresql dialect

--- src/main/java/scanner/db/CustomPostgresDialect.java
+++ src/main/java/scanner/db/CustomPostgresDialect.java
@@ -3,7 +3,7 @@ package scanner.db;
 import org.hibernate.dialect.PostgreSQL9Dialect;
 import org.hibernate.dialect.PostgreSQLDialect;
 
-public class CustomPostgresDialect extends PostgreSQLDialect {
+public class CustomPostgresDialect extends PostgreSQL9Dialect {
     public CustomPostgresDialect() {
         registerFunction("fts", new PostgreSQLFullTextSearchFunction());
     }
