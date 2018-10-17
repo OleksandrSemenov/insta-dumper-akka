8b24a67ae8651aaec7dcbae072d023963c930969
chuiykPasha <p4y10k@gmail.com>
2018-10-17 10:57:04 +0300

    created new RestController Scan

---
+++ src/main/java/scanner/rest/ScanRestController.java
@@ -0,0 +1,25 @@
+package scanner.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import scanner.Scanner;
+import scanner.rest.exception.ScannerAlreadyWorksException;
+
+@RestController
+@RequestMapping("/api/scan/")
+public class ScanRestController {
+    @Autowired
+    private Scanner scanner;
+
+    @RequestMapping(value = "/start-scan/{name}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void startScan(@PathVariable("name") String name){
+        if(scanner.isScannerWork()){
+            throw new ScannerAlreadyWorksException();
+        }
+
+        scanner.startScanWithName(name);
+    }
+}
